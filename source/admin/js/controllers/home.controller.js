28b3dcb415dd8b8e45a80277e51b66fbce72995f
Adam Carnagey <adam@carnagey.com>
2015-06-07 19:14:04 -0500

    Hooking up real data to the charts.

--- source/admin/js/controllers/home.controller.js
+++ source/admin/js/controllers/home.controller.js
@@ -1,6 +1,41 @@
 myApp.controller('HomeController',
   function($scope, $rootScope, REST_URL, $location, $http) {
     $rootScope.active = { "home" : "active", "users" : "", "registrations" : "",  "courseRequests" : "" };
-    $scope.options = [[0, 3], [4, 8], [8, 5], [9, 13]];
+    //$scope.options = [[0, 3], [4, 8], [8, 5], [9, 13]];
+
+    $http({
+      method: 'GET',
+      url: REST_URL + '/report/monthlyByDay',
+    }).then(function(dataResponse) {
+      $scope.response = dataResponse.data.data;
+      console.log($scope.response);
+      $scope.options = {
+        title: {
+          text: 'Daily Stats By Month',
+        },
+        subtitle: {
+          text: $scope.response.subtitle,
+        },
+        xAxis: {
+            title: { text: 'Days of Month' },
+            categories: $scope.response.daysInMonth
+        },
+        yAxis: {
+            min: 0,
+            allowDecimals: false,
+            title: {
+                text: 'Total'
+            }
+        },
+        series: [{
+            name: 'Registrations By Day',
+            data: $scope.response.registrationsByDay
+        },{
+            name: 'Course Requests By Day',
+            data: $scope.response.courseRequestsByDay
+        }]
+      };
+      console.log($scope.options);
+    });
   }
 );
--- source/admin/js/directives/highchart.directive.js
+++ source/admin/js/directives/highchart.directive.js
@@ -1,35 +1,30 @@
-myApp.directive('myChart', function() {
+myApp.directive('myChart', function($timeout) {
   return function(scope, element, attrs) {
-
-    var options = {
-      title: {
-        text: 'Total Registered by Day',
-      },
-      subtitle: {
-        text: 'April 2015',
-      },
-      xAxis: {
-          title: { text: 'Days of Month' },
-          categories: ['1', '2', '3', '4', '5', '6',
-          '7', '8', '9', '10', '11', '12', '13', '14',
-          '15', '16', '17', '18', '19', '20', '21', '22',
-          '23', '24', '25', '26', '27', '28', '29', '30']
-      },
-      yAxis: {
-          title: {
-              text: 'Total Registered'
-          }
-      },
-      series: [{
-          name: 'Total Registered Per Day',
-          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2,
-          null, null, null, null, null, null, null, null, null, null,
-          null, null, null, null, null, null, null, null]
-      }]
-    };
-
     // apply the plugin
-    var dataTable = element.highcharts(options);
+    scope.chart = element.highcharts(scope.options);
 
+    scope.$watch(attrs.data, function(value) {
+      var val = value || null;
+      if (val) {
+          // var complete = function (options) {
+              scope.chart = element.highcharts(scope.options);
+              // capture all available series
+              // var allSeries = chart.series;
+              // for (var i = 0; i < allSeries.length; i++) {
+              //     allSeries[i].setData(options.series[i].data, false);
+              // }
+
+              //scope.chart.redraw();
+          // };
+
+          // doesn't work without the timeout
+          // $timeout(function() {
+          //     Highcharts.data({
+          //         table: config.data.table,
+          //         complete: complete
+          //     });
+          // }, 0);
+      }
+    });
   };
 });
